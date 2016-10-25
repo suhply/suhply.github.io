9a625eb617bab729f72ff5be1ebf44f8e9d0a5e5
Vikram Vetrivel <vikram.1982@gmail.com>
2016-10-25 15:37:51 +1100

    Angular - Product Grid and Hero Image is working

--- js/ng/app/landingPage/ngLandingPage.js
+++ js/ng/app/landingPage/ngLandingPage.js
@@ -5,37 +5,35 @@ landingPageApp.service('productsService', ['$http', function($http) {
 
     this.getProducts = function() {
         var baseurl = "https://dl.dropboxusercontent.com/u/57271664/testbed/json/"; //TODO: Replace with actual API
-        url = baseurl + "f/sliderImages.json";
+        url = baseurl + "products.json";
         return $http.get(url);
     };
 }]);
 
-landingPageApp.controller('landingPageController', ['$scope', '$http', 'productsService', function($scope, $http, productsService) {
+landingPageApp.controller('productController', ['$scope', '$http', 'productsService', function($scope, $http, productsService) {
 
-    $scope.heroimage = "img/sampleImages/jeans3.jpg";
+    $scope.selectedCategory = "Jeans";
+    
     getProducts();
 
     function getProducts() {
         productsService.getProducts()
             .then(function(response) {
                 var productData = response.data;
-                $scope.sliderImages = {};
-                $scope.sliderImages = productData.sliderImages;
+                console.log(productData);
+
+                $scope.products = productData[$scope.selectedCategory];
+                $scope.hero = $scope.products[0];
+
+                console.log ($scope.products);
+
             }
         );
     }
+
+
+    $scope.selectProduct = function(product) {
+        $scope.hero = product;
+    }
 }]);
 
-
-landingPageApp.directive ('backImg', function() {
-
-    return function (scope, element, attrs) {
-        var url = attrs.backImg;
-        element.css ({
-            'background' : 'url(' + url +')  no-repeat center',
-            
-            'width':'100%',
-            'height':'100%'
-        });
-    };
-});
